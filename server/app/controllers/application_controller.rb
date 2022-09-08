765c1f78404cd9a98ba80d46e6f073e06e48aa9c
loretta dhahabu <loreydjefwa@gmail.com>
2022-09-08 15:51:04 +0300

    add end points

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -18,94 +18,35 @@ class ApplicationController < Sinatra::Base
     200
   end
 
-
-    #get products
-
-        get '/products/:id' do
-            get_product_by_id = Product.find(params[:id])
-            get_product_by_id.to_json
-        end
-
-        get "/product" do
-          all_products = Product.all
-          all_products.to_json
-        end
-
-    # #get reviews
-
-    #     get '/specializations/:id' do
-    #         spec = Specialization.find(params[:id])
-    #         spec.to_json
-    #     end
-
-    #     get "/specializations" do
-    #         specs = Specialization.all
-    #         specs.to_json
-    #     end
-
-    # #get users
-
-    #     get '/pets/:id' do
-    #         pet = Pet.find(params[:id])
-    #         pet.to_json
-    #     end
-     
-    #     get "/pets" do
-    #         pets = Pet.all
-    #         pets.to_json
-    #     end
-     
-
-    # #post
-    #     post '/doctors' do
-    #         daktari = Doctor.create(  
-    #         name: params[:name], 
-    #         location: params[:location],
-    #         license_number: params[:license_number], specialization: params[:specialization],phone_number: params[:phone_number],picture_link: params[:picture_link],years_of_experience: params[:years_of_experience])
-    #         daktari.to_json
-    #     end
-
-    #   post '/specialization' do
-    #     spec = Specialization.create(
-    #       field_of_speciality: params[:field_of_speciality],
-    #       doctor_id: params[:doctor_id],
-    #       pet_id: params[:pet_id]
-    #     )
-    #     spec.to_json
-    #   end
-
-    #   post '/pets' do
-    #     pet = Pet.create(
-    #       name: params[:name],
-    #       pet_type: params[:pet_type],
-    #       age: params[:age],
-    #       color: params[:color],
-    #       specialization: params[:specialization],
-    #       tag_number: params[:tag_number],
-    #       pets_breed: params[:pets_breed],
-    #       phone_number: params[:phone_number],
-    #     )
-    #     pet.to_json
-    #   end
-
-
-    # #delete
-
-    #     delete '/doctors/:id' do
-    #         delete_doc = Doctor.find(params[:id])
-    #         delete_doc.destroy
-    #         delete_doc.to_json
-    #     end
-
-    #     delete '/specializations/:id' do
-    #         spec = Specialization.find(params[:id])
-    #         spec.destroy
-    #         spec.to_json
-    #     end
-
-    #     delete '/pets/:id' do
-    #         pet = Pet.find(params[:id])
-    #         pet.destroy
-    #         pet.to_json
-    #     end
+    # default route products
+    get "/products" do
+      products = Product.all
+      products.to_json
+    end
+    # get product by id
+    get "/products/:id" do
+      products = Product.find(params[:id])
+      products.to_json
+    end
+    # get all reviews
+    get "/reviews" do
+      reviews = Review.all
+      reviews.to_json
+    end
+    # get review by id
+    get "/reviews/:product_id" do
+      review = Review.find(params[:product_id])
+      review.to_json
+    end
+    # get all users
+    get "/users" do
+      users = User.all
+      users.to_json
+    end
+    # get user by id
+    get "/users/:id" do
+      user = User.find(params[:id])
+      user.to_json
+    end
+    
 end
