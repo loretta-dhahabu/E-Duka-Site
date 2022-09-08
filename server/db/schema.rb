47aada1f471dfb6fa6672b1158de212aab6f5ff3
loretta dhahabu <loreydjefwa@gmail.com>
2022-09-08 13:13:21 +0300

    seeding supplier data

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_07_211600) do
+ActiveRecord::Schema.define(version: 2022_09_08_090902) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.string "description"
     t.float "price"
     t.string "image_url"
+    t.integer "supplier_id"
   end
 
   create_table "reviews", force: :cascade do |t|
